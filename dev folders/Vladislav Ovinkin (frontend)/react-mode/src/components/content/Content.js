d4801bf4fd6d1ed78e5986448521cf5783a45840
Vladislav <vladovinkin@gmail.com>
2020-04-23 22:50:59 +0300

    Add Route's to Switch

--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/content/Content.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/content/Content.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './content.css';
+
 
 function Content() {
     return (
-      <div className="content-main">
-
-      </div>
+        <div className="content-main">
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/about">
+                        it's about
+                    </Route>
+                    <Route path="/toBusiness">
+                        it's toBusiness
+                    </Route>
+                    <Route path="/toFinorg">
+                        it's toFinorg
+                    </Route>
+                    <Route path="/">
+                        it's index
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
     );
 }
 
