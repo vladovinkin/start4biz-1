b9e269dd81d55226fc24eb40afbef5cab3cf70cf
Vladislav <vladovinkin@gmail.com>
2020-04-23 20:09:03 +0300

    Подключил в приложение компонент Content

--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/App.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Content from './components/content/Content';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="container">
       <Header />
-      
+      <Content />
       <Footer />
     </div>
   );
