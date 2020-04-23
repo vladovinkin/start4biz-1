c9ca3be62755b74213d37eb0c44fc2f5bc19cc6f
Vladislav <vladovinkin@gmail.com>
2020-04-23 19:59:58 +0300

    Удалил двойную контейнеризацию
    в контейнерах footer и header

--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/footer/Footer.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/footer/Footer.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import './Footer.css';
+import './footer.css';
 import './../../font-awesome.min.css';
 
 function Footer() {
     return (
-      <div className="main-footer">
-        
-      </div>
+        <footer className="main-footer">
+            <div className="footer-info">
+                <div className="footer-info-left">
+                    <ul className="footer-info-left-list">
+                        <li className="footer-info-left-item"><a href="about-contacts.html#about" className="footer-info-left-link">О проекте</a></li>
+                        <li className="footer-info-left-item"><a href="2finorg.html" className="footer-info-left-link">Финансовым организациям</a></li>
+                        <li className="footer-info-left-item"><a href="2business.html" className="footer-info-left-link">Бизнесу</a></li>
+                        <li className="footer-info-left-item"><a href="about-contacts.html#contacts" className="footer-info-left-link">Контакты</a></li>
+                    </ul>
+                </div>
+                <div className="footer-info-right">
+                    <ul className="footer-info-right-list">
+                        <li className="footer-info-right-item"><a href="#" className="footer-info-right-link">Политика конфиденциальности</a></li>
+                        <li className="footer-info-right-item"><a href="#" className="footer-info-right-link">Пользовательское соглашение</a></li>
+                        <li className="footer-info-right-item"><a href="#" className="footer-info-right-link">Согласие на обработку персональных данных</a></li>
+                        <li className="footer-info-right-item"><a href="#" className="footer-info-right-link">Форма обратной связи</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="footer-links">
+                <div className="footer-social-links-block">
+                    <a href="#" className="footer-social-link" title="Мы во вконтакте"><i className="fa fa-vk fa-lg"></i></a>
+                    <a href="#" className="footer-social-link" title="Наш инстаграмм"><i className="fa fa-instagram fa-lg"></i></a>
+                    <a href="#" className="footer-social-link" title="Наш канал на youtube"><i className="fa fa-youtube fa-lg"></i></a>
+                    <a href="#" className="footer-social-link" title="Мы в facebook"><i className="fa fa-facebook-square fa-lg"></i></a>
+                </div>  
+                <div className="footer-contacts">
+                    <a className="footer-email" href="mailto:info@start4.biz" title="Написать письмо">info@start4.biz</a>
+                    <a className="footer-phone" href="tel:+79111299351" title="Просто позвоните нам!">+7 911 129-93-51</a>
+                </div>
+            </div>
+        </footer>
     );
   }
   
--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/header/Header.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/header/Header.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import './Header.css';
+import './header.css';
+import logoImage from './img/pre-logo.png';
 
 function Header() {
     return (
-      <div className="main-header">
-        
-      </div>
+    <header className="main-header">
+        <div className="header-up">
+            <div className="header-up-left">
+                <span className="header-up-left-adress">
+                    Санкт-Петербург, пл. Карла Фаберже, д8 
+                </span>
+            </div>
+            <a href="index.html" className="header-up-logo-link" title="На главную страницу">
+                <img className="header-up-logo" src={logoImage} width="40" height="30" alt="Start4.biz logo" />
+            </a>
+            <div className="header-up-right">
+                <a className="header-up-right-email" href="mailto:info@start4.biz" title="Написать письмо">info@start4.biz</a>
+                <a className="header-up-right-phone" href="tel:+79111299351" title="Просто позвоните нам!">+7 911 129-93-51</a>
+            </div>
+        </div>
+        <div className="header-down">
+            <nav className="header-down-left-menu">
+                <ul className="header-down-left-menu-list">
+                    <li className="header-down-left-menu-item"><a href="about-contacts.html#about" className="header-down-left-menu-link">О проекте</a></li>
+                    <li className="header-down-left-menu-item"><a href="2business.html" className="header-down-left-menu-link">Бизнесу</a></li>
+                    <li className="header-down-left-menu-item"><a href="2finorg.html" className="header-down-left-menu-link">Финансовым организациям</a></li>
+                    <li className="header-down-left-menu-item"><a href="about-contacts.html#contacts" className="header-down-left-menu-link">Контакты</a></li>
+                </ul>
+            </nav>
+            <a className="header-down-lk" href="#">Личный кабинет</a>
+            <div className="header-down-right-menu">
+                <ul className="header-down-right-menu-list">
+                    <li className="header-down-right-menu-item"><a href="#" className="header-down-right-menu-link">Кредиты</a></li>
+                    <li className="header-down-right-menu-item"><a href="#" className="header-down-right-menu-link">Эквайринг</a></li>
+                    <li className="header-down-right-menu-item"><a href="#" className="header-down-right-menu-link">Овердрафты</a></li>
+                    <li className="header-down-right-menu-item"><a href="#" className="header-down-right-menu-link">Лизинг</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
     );
   }
   
