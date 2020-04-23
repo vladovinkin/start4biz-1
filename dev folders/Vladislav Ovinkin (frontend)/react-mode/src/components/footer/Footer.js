354e2fc3eaea290c96abf1e2bfea09c3de422086
Vladislav <vladovinkin@gmail.com>
2020-04-23 12:07:47 +0300

    Добавил компонент Footer
    шаблон кода и заполненные стили

---
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+import './../../font-awesome.min.css';
+
+function Footer() {
+    return (
+      <div className="main-footer">
+        
+      </div>
+    );
+  }
+  
+  export default Footer;
