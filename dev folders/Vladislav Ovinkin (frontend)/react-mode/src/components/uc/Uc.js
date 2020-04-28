a870e0d52e60d64f03080469959e8c4c5e4476ab
Vladislav <vladovinkin@gmai.com>
2020-04-29 01:12:30 +0300

    Создал комопонент-заглушку
    This page is UNDER CONSTRUCTION

---
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/uc/Uc.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './uc.css';
+
+function Uc() {
+    return (
+        <div className="uc">
+            <p className="uc-p">This</p>
+            <p className="uc-p">page</p>
+            <p className="uc-p">is</p>
+            <p className="uc-p">UNDER CONSTRUCTION</p>
+        </div>
+    );
+}
+
+export default Uc;
+
