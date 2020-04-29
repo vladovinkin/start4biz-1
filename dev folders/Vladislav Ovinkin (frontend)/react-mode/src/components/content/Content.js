cc2a748d4cc5efcaa948fcde0fcdae2c77442761
Vladislav <vladovinkin@gmail.com>
2020-04-29 22:12:18 +0300

    подключил компоненты 1 и 4 блоков главной страницы

--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/content/Content.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/content/Content.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Uc from './../uc/Uc';
 import ToBusiness from './../toBusiness/ToBusiness';
 import ToFinorg from './../toFinorg/ToFinorg';
-import About from './../about/About';
+import About from '../About/About';
+import BestDealBlock from './../BestDealBlock/BestDealBlock';
+import PartnersBlock from './../PartnersBlock/PartnersBlock';
 import './content.css';
 
 function Content() {
@@ -34,7 +36,10 @@ function Content() {
                     <Uc />
                 </Route>
                 <Route path="/">
-                    {/* <Main /> */}
+                    <Fragment>
+                        <BestDealBlock />
+                        <PartnersBlock />
+                    </Fragment>
                 </Route>
             </Switch>
         </div>
--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/index.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './store/reducers/rootReducer';
+import App from './App';
 import './index.css';
 import './normalize.css';
-import App from './App';
+
+const store = createStore(rootReducer, compose(
+	applyMiddleware(thunk),
+//	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
