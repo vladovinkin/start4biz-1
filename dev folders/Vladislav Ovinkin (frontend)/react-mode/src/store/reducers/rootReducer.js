9a2a34abf1f5df66594d01c8b68e352182734820
Vladislav <vladovinkin@gmail.com>
2020-04-30 01:23:03 +0300

    Подключил reducer Михаила к общему хранилищу

--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/store/reducers/rootReducer.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/store/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import {reducer as proposalFormReduxReducer} from 'redux-form';
 import {proposalFormReducer} from './proposalFormReducer';
 import {popupReducer} from './popupReducer';
 import {partnersReducer} from './partnersReducer';
+import {booleanReducer} from './booleanReducer';
 
 export const rootReducer = combineReducers({
     proposalForm: proposalFormReducer,
     form: proposalFormReduxReducer,
     popup: popupReducer,
     partners: partnersReducer,
+    bool: booleanReducer,
 })
