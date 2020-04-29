bfe9c03ceb0136812d1ba46a830f0cd6ecea3141
Vladislav <vladovinkin@gmai.com>
2020-04-29 11:40:48 +0300

    Обновил раздел "Финансовым организациям"
    компонент ToFinorg

--- dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/toFinorg/ToFinorg.js
+++ dev folders/Vladislav Ovinkin (frontend)/react-mode/src/components/toFinorg/ToFinorg.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './toFinorg.css';
-import toFinorgImage from './../../img/rectangle-335х426.png';
 import coinsImage from './../../img/coins.png';
 import monitorImage from './../../img/monitor.png';
 import commentImage from './../../img/comment.png';
@@ -10,77 +10,79 @@ function ToFinorg() {
     return (
         <>
             <section className="finorg-info-block">
-                <div className="finorg-left-block">
-                    <h2 className="to-business-header">Финансовым организациям</h2>
-                    <p className="to-business-text">Портал start4.biz позволяет быстро и удобно получить выгодные предложения по ряду продуктов от ведущих лизинговых и финансовых компаний.</p>
-                    <p className="to-business-text">Мы будем рады видеть Вас в качестве компании-партнера, если Вы имеете лицензию ЦБ на предоставление финансовых услуг юридическим лицам и предоставляете следующие услуги :</p>
-                    <ul className="to-business-list">
-                        <li className="to-business-item">Кредитование Юридических лиц и Индивидуальных предпринимателей</li>
-                        <li className="to-business-item">Предоставление банковских гарантий для работы с 44-ФЗ и 223-ФЗ</li>
-                        <li className="to-business-item">Лизинг транспорта или оборудования</li>
-                        <li className="to-business-item">Предоставление овердрафтов для покрытия кассовых разрывов</li>
-                        <li className="to-business-item">Эквайринг</li>
-                        <li className="to-business-item">Иные финансовые услуги</li>
-                    </ul>
-                </div>
-                <div className="finorg-right-block">
-                    <img src={toFinorgImage} alt="Финансовым организациям" width="335" height="426" className="finorg-info-block-image" />
-                    <div className="finorg-be-partner-button">
-                        <a href="#" className="page-button be-partner-button">
-                            <span className="page-button-text be-partner-button-text">
-                                Стать партнёром
-                            </span>
-                        </a>
+                <h2 className="to-finorg-header">Финансовым организациям</h2>
+                <div className="finorg-decription">
+                    <div className="finorg-left-block">
+                        <p className="to-business-text">Портал start4.biz позволяет быстро и удобно получить выгодные предложения по ряду продуктов от ведущих лизинговых и финансовых компаний.</p>
+                        <p className="to-business-text">Мы будем рады видеть Вас в качестве компании-партнера, если Вы имеете лицензию ЦБ на предоставление финансовых услуг юридическим лицам и предоставляете следующие услуги :</p>
+                        
                     </div>
+                    <div className="finorg-right-block">
+                        <ul className="to-business-list">
+                            <li className="to-business-item">Кредитование Юридических лиц и Индивидуальных предпринимателей</li>
+                            <li className="to-business-item">Предоставление банковских гарантий для работы с 44-ФЗ и 223-ФЗ</li>
+                            <li className="to-business-item">Лизинг транспорта или оборудования</li>
+                            <li className="to-business-item">Предоставление овердрафтов для покрытия кассовых разрывов</li>
+                            <li className="to-business-item">Эквайринг</li>
+                            <li className="to-business-item">Иные финансовые услуги</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="finorg-be-partner-button">
+                    <Link to="/uc" className="page-button-fin be-partner-button-fin">
+                        <span className="page-button-text be-partner-button-text">
+                            Стать партнёром
+                        </span>
+                    </Link>
                 </div>
             </section>
             <div className="h-line"></div>
             <section className="why-we">
-                <h3 className="why-we-header">Почему мы ?</h3>
-                <ul className="why-we-list why-we-list-finorg">
-                    <li className="why-we-item why-we-item-finorg">
+                <h2 className="to-finorg-header">Почему мы ?</h2>
+                <div className="finorg-decription">
+                    <div className="why-we-item">
                         <figure className="why-we-article">
-                            <img src={coinsImage} width="110" height="110" alt="Стабильные заявки" className="why-we-article-img" />
+                            <img src={coinsImage} alt="Стабильные заявки" className="why-we-article-img" />
                             <figcaption className="why-we-caption">
                                 <h4 className="why-we-article-header">Стабильные заявки</h4>
                                 <p className="why-we-article-text">Предоставляем стабильный поток заявок на финансовые продукты</p>
                             </figcaption>
                         </figure>
-                    </li>
-                    <li className="why-we-item why-we-item-finorg">
+                    </div>
+                    <div className="why-we-item">
                         <figure className="why-we-article">
-                            <img src={monitorImage} width="110" height="110" alt="Взаимодействие" className="why-we-article-img" />
+                            <img src={monitorImage} alt="Взаимодействие" className="why-we-article-img" />
                             <figcaption className="why-we-caption">
-                                <h4 className="why-we-article-header"Взаимодействие>Взаимодействие</h4>
+                                <h4 className="why-we-article-header">Взаимодействие</h4>
                                 <p className="why-we-article-text">В едином интерфейсе Вы сможете взаимодействовать с заявками</p>
                             </figcaption>
                         </figure>
-                    </li>
-                    <li className="why-we-item why-we-item-finorg">
+                    </div>
+                    <div className="why-we-item">
                         <figure className="why-we-article">
-                            <img src={commentImage} width="110" height="110" alt="Коммуникация" className="why-we-article-img" />
+                            <img src={commentImage} alt="Коммуникация" className="why-we-article-img" />
                             <figcaption className="why-we-caption">
                                 <h4 className="why-we-article-header">Коммуникация</h4>
                                 <p className="why-we-article-text">Вы получаете новый канал общения с бизнесом</p>
                             </figcaption>
                         </figure>
-                    </li>
-                    <li className="why-we-item why-we-item-finorg">
+                    </div>
+                    <div className="why-we-item">
                         <figure className="why-we-article">
-                            <img src={clockImage} width="110" height="110" alt="Скорость" className="why-we-article-img" />
+                            <img src={clockImage} alt="Скорость" className="why-we-article-img" />
                             <figcaption className="why-we-caption">
                                 <h4 className="why-we-article-header">Скорость</h4>
                                 <p className="why-we-article-text">Быстро реагируем на ваши запросы</p>
                             </figcaption>
                         </figure>
-                    </li>
-                </ul>
-                <div className="why-we-under">
-                    <a href="#" className="page-button be-partner-button">
+                    </div>
+                </div>
+                <div className="finorg-be-partner-button">
+                    <Link to="/uc" className="page-button-fin be-partner-button-fin">
                     <span className="page-button-text be-partner-button-text">
                         Стать партнёром
                     </span>
-                    </a>
+                    </Link>
                 </div>
         </section>
         </>
